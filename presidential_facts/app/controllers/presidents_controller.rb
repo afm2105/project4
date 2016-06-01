fd48da24ea94336a39cdc27e633d9f40b07336b1
antony mastroniani <afm2105@gmail.com>
2016-06-01 17:21:26 -0400

    got new ebtries to show

--- presidential_facts/app/controllers/presidents_controller.rb
+++ presidential_facts/app/controllers/presidents_controller.rb
@@ -1,4 +1,8 @@
 class PresidentsController < ApplicationController
+  def index
+    @presidents = President.all
+  end
+
   def show
     @president = President.find(params[:id])
   end
@@ -7,15 +11,15 @@ class PresidentsController < ApplicationController
   end
 
   def create
-@president = President.new(president_params)
-@president.save
-redirect_to @president
-end
+    @president = President.new(president_params)
+    @president.save
+    redirect_to @president
+  end
 
-private
-def article_params
-  params.require(:article).permit(:name, :text)
-end
+  private
+  def president_params
+    params.require(:president).permit(:name, :text)
+  end
 
   #   @president = President.new(params.require(:president).permit(:name, :text)
   #   # render plain: params[:president].inspect
